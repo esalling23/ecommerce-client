4d4b207be1ffaafcd4dff47e00a2049300122c5b
esalling23 <esalling23@gmail.com>
2021-08-27 15:13:10 -0400

    Adds LinkButton component
    Gratefully borrowed from stackoverflow answer:
    https://stackoverflow.com/a/49439893/15835070

---
+++ src/components/shared/LinkButton.js
@@ -0,0 +1,27 @@
+// https://stackoverflow.com/a/49439893/15835070
+// unclear if this is an accessible solution, see https://stackoverflow.com/a/58846293/15835070
+import React from 'react'
+import { withRouter } from 'react-router'
+
+const LinkButton = ({
+  history,
+  location,
+  match,
+  staticContext,
+  to,
+  onClick,
+  // ⬆ filtering out props that `button` doesn’t know what to do with.
+  ...rest
+}) => {
+  return (
+    <button
+      {...rest} // `children` is just another prop!
+      onClick={(event) => {
+        onClick && onClick(event)
+        history.push(to)
+      }}
+    />
+  )
+}
+
+export default withRouter(LinkButton)
