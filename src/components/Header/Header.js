2f3dea09ede64337e97ef722f1266628a681a670
esalling23 <esalling23@gmail.com>
2021-08-27 15:11:31 -0400

    Restyles Header
    Moves Account & cart to the right
    Removes product link
    Moves "hello -- " message to the left

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,7 +21,6 @@ const unauthenticatedOptions = (
 
 const alwaysOptions = (
   <Fragment>
-    <NavLink exact to='/' className='nav-link'>Products</NavLink>
   </Fragment>
 )
 
@@ -32,9 +31,9 @@ const Header = ({ user }) => (
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
-      <Nav className='ml-auto'>
+      <Nav className='ms-auto w-100 justify-content-end'>
         {user && (
-          <span className='navbar-text mr-2'>Welcome, {user.email}</span>
+          <span className='navbar-text me-auto'>Welcome, {user.email}</span>
         )}
         {alwaysOptions}
         {user ? authenticatedOptions : unauthenticatedOptions}
