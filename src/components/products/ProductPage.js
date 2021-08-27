01515da7bbb7509ffbad2321ee9240f9feda356b
esalling23 <esalling23@gmail.com>
2021-08-27 09:30:09 -0400

    Styles product page
    Adds fluid image element
    Adds padding class to description
    Splits page into two sections
    Adds mobile-first styling for flex layout on large/small screens

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -1,8 +1,34 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 import { withRouter } from 'react-router-dom'
 
 import { AddToCartButton } from '../styled/Buttons'
 import { showProduct } from '../../api/products'
+
+import Image from 'react-bootstrap/Image'
+
+const StyledPage = styled.div`
+  display: flex;
+  flex-wrap: wrap-reverse;
+  flex-direction: row;
+  flex-grow: 0;
+  justify-content: center;
+  margin: 2em;
+
+  @media (min-width: 768px) {
+    flex-wrap: nowrap;
+  }
+`
+
+const PageSection = styled.div`
+  flex: auto;
+  margin: 1rem;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    width: 40%;
+  }
+`
 
 const ProductPage = ({ match, msgAlert, addToCart }) => {
   const [product, setProduct] = useState(null)
@@ -27,15 +53,21 @@ const ProductPage = ({ match, msgAlert, addToCart }) => {
   }
 
   return (
-    <>
-      <h3>{product.title}</h3>
-      <h4>Price: ${product.price}</h4>
-      <img src="https://res.cloudinary.com/esalling/image/upload/v1518704988/few_dots.jpg"/>
-      <p>{product.description}</p>
-      <div style={{ width: '18rem' }}>
+    <StyledPage>
+      <PageSection>
+        <Image
+          fluid
+          className="w-100"
+          src="https://res.cloudinary.com/esalling/image/upload/v1518704988/few_dots.jpg"
+        />
+        <p className="py-4">{product.description}</p>
+      </PageSection>
+      <PageSection>
+        <h3 className="text-display">{product.title}</h3>
+        <h4>Price: ${product.price}</h4>
         <AddToCartButton onClick={() => addToCart(match.params.id)}>Add to cart</AddToCartButton>
-      </div>
-    </>
+      </PageSection>
+    </StyledPage>
   )
 }
 
