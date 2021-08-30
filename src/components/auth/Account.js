11153a9678264995989c8f518a9ab5f11ee5f20e
esalling23 <esalling23@gmail.com>
2021-08-30 14:14:12 -0400

    Major changes to Account page
    Uses tabs to organize different account information
    in preparation for OrderHistory implementation

--- src/components/auth/Account.js
+++ src/components/auth/Account.js
@@ -1,20 +1,87 @@
 import React from 'react'
+import styled from 'styled-components'
+import { lighten } from 'polished'
 import { withRouter } from 'react-router-dom'
-import LinkButton from '../shared/LinkButton'
 
+import { primary, light } from '../styled/colors'
+import Link from '../styled/Links'
+// import AuthenticatedRoute from '../AuthenticatedRoute/AuthenticatedRoute'
+import OrderHistory from '../orders/OrderHistory'
+// import OrderDetail from '../orders/OrderDetails'
+
+import Tab from 'react-bootstrap/Tab'
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+// import LinkButton from '../shared/LinkButton'
 import ChangePassword from './ChangePassword'
 
+const TabLink = styled(Nav.Link)`
+  margin: 0.25em 0;
+
+  &:hover, &:focus {
+    background: ${lighten(0.7, primary)};
+    cursor: pointer;
+  }
+`
+
+// For some reason Tab.Container won't recognize my styles (it's a fluid container)
+// Using a wrapper component instead
+const TabContainer = styled.div`
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    width: 80%;
+  }
+`
+
 // Contains change password, sign out, order history, preferences...
-const Account = () => {
+const Account = (props) => {
   return (
-    <div className='row'>
-      <div className='col-sm-10 col-md-8 mt-5'>
-        <ChangePassword/>
-      </div>
-      <div className='col-sm-2 col-md-4 mt-5'>
-        <LinkButton to='/sign-out'>Sign Out</LinkButton>
-      </div>
-    </div>
+    <TabContainer>
+      <Tab.Container className="border-soft" defaultActiveKey="settings">
+        <Row>
+          <Col className="mt-5" sm={3} style={{ borderRight: `1px solid ${light}` }}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <TabLink eventKey="settings">Account Setting</TabLink>
+              </Nav.Item>
+              <Nav.Item>
+                <TabLink
+                  eventKey="orderHistory"
+                >Order History</TabLink>
+              </Nav.Item>
+              <Nav.Item className="text-center mt-2">
+                <Link to='/sign-out'>Sign Out</Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col className="mt-5" sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="settings">
+                <>
+                  <h4>Update your data:</h4>
+                  <hr/>
+                  <ChangePassword/>
+                </>
+              </Tab.Pane>
+              <Tab.Pane eventKey="orderHistory">
+                <>
+                  <h4>Order history:</h4>
+                  {/* <AuthenticatedRoute
+                    path={props.match.path + '/orders/:id'}
+                    exact
+                    render={() => <OrderDetail {...props}/>}
+                  /> */}
+                  <OrderHistory {...props}/>
+                </>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </TabContainer>
   )
 }
 
