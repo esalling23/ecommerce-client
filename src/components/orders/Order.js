6ad2f3d087049093dec7a7e13415fe6bbe481dc9
esalling23 <esalling23@gmail.com>
2021-09-05 16:50:27 -0400

    Adds remove option for products in cart

--- src/components/orders/Order.js
+++ src/components/orders/Order.js
@@ -14,29 +14,36 @@ const StyledOrder = styled.div`
   }
 `
 
+const RemoveLink = styled(Link)`
+  display: block;
+  font-size: 0.75em;
+  margin-top: 0.5em;
+`
+
 const Order = ({ total, products, removeFromCart, updateProductInCart }) => {
   return (
-  <StyledOrder className="p-2">
-    <h2>Current Cart:</h2>
-    <ListGroup>
-      {products.map(({ productRef, count }, i) => (
-        <ListGroup.Item
-          key={i}
-          variant='info'
-        >
+    <StyledOrder className="p-2">
+      <h2>Current Cart:</h2>
+      <ListGroup>
+        {products.map(({ productRef, count }, i) => (
+          <ListGroup.Item
+            key={i}
+            variant='info'
+          >
             <span>{productRef.title} - ${productRef.price} each</span>
             <CountForm
               prodId={productRef._id}
               count={count}
               updateProductInCart={updateProductInCart}
             />
-          <span className="order-price">${(parseFloat(productRef.price) * count).toFixed(2)}</span>
-        </ListGroup.Item>
-      ))}
-      <ListGroup.Item className="order-price" style={{ fontWeight: 'bold' }} variant='warning'>Total: ${total}</ListGroup.Item>
-    </ListGroup>
-  </StyledOrder>
-)
+            <span className="order-price">${(parseFloat(productRef.price) * count).toFixed(2)}</span>
+            <RemoveLink onClick={() => removeFromCart(productRef._id)}>Remove from cart</RemoveLink>
+          </ListGroup.Item>
+        ))}
+        <ListGroup.Item className="order-price" style={{ fontWeight: 'bold' }} variant='warning'>Total: ${total}</ListGroup.Item>
+      </ListGroup>
+    </StyledOrder>
+  )
 }
 
 export default Order
